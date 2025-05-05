ead0615806c0c260b94e4d4bb7d46ce3d6d71778
S4mu3l-Silv4 <samuelssone02@gmail.com>
2025-05-05 19:20:15 -0300

    Adicionando nova funcionalidade na navbar

--- assets/js/script-home.js
+++ assets/js/script-home.js
@@ -1,15 +1,16 @@
 // Navbar com scroll inteligente:
+
 let lastScrollTop = 0
 const navbar = document.getElementById("navbar")
 
 window.addEventListener("scroll", function () {
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop
 
-    if (scrollTop < lastScrollTop) {
-        navbar.classList.remove("hidden")
+    if (scrollTop > lastScrollTop) {
+        navbar.style.top = "-70px"
     } else {
-        navbar.classList.add("hidden")
+        navbar.style.top = "0"
     }
 
-    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop
 })
